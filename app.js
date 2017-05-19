788716d2fe671d094bd89450d79a7ab6283a9d59
Tang Wei <tangwei@marketengine.cn>
2017-05-19 22:07:43 +0800

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var Koa=require('koa');
+var sha1=require('sha1');
+var config={
+    wechat:{
+        appId:'wxd97732294d081c1d',
+        appSecret:'ec2d96a460c123c89cd8855309f42512',
+        token:'wechatnodejs'
+    }
+};
+var app=new Koa();
+
+app.use(function *(next){
+    //this.body = 'Hello World';
+
+     console.log(this.query);
+    // var token=config.token;
+    // var signature=this.query.signature;
+    // var nonce=this.query.nonce;
+    // var timestamp=this.query.timestamp;
+    // var echostr=this.query.echostr;
+    // var str=[token,timestamp,nonce].sort().join('');
+    // var sha=sha1(str);
+    // if(sha===signature){
+    //     this.body=echostr+'';
+    // }else{
+    //     this.body='wrong';
+    // }
+
+});
+app.listen(3000);
+console.log('listening:3000');
